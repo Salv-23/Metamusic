b66cd628f415f92a02902a7c6f8dba1983883299
Salvador Estrella <seodent.qro@gmail.com>
2022-10-12 11:28:48 -0500

    feat!: First complete release of MusicMetadata class
    MusicMetaData can retrieve artist, release and title information. It
    still misses complete documentation and proper handling of exceptions.

--- metamusic/clients/musicbrainz.py
+++ metamusic/clients/musicbrainz.py
@@ -6,34 +6,11 @@ Purpose: Query Metadata from MusicBrainz API
 """
 
 import requests
-from time import sleep
 
 
 # --------------------------------------------------
 class MusicMetadata:
     base_url = "http://musicbrainz.org/ws/2/"
-
-    @classmethod
-    def _pick_artist(cls, artist_list: list[dict]):
-        # TODO: Deal with artists that don't return a score equal to 100.
-        artist = None
-        for posible_artist in artist_list:
-            if not artist:
-                artist = posible_artist
-            elif artist["score"] < posible_artist["score"]:
-                artist = posible_artist
-        if artist and artist["score"] != 100:
-            raise NotImplementedError(
-                "Selection of artists with lower score than 100 not implemented yet"
-            )
-        return artist
-
-    @classmethod
-    def _artist_lookup(cls, artist_id: str) -> dict:
-        artist_lookup = f"{cls.base_url}artist/{artist_id}?inc=release-groups&fmt=json"
-        artist_response = requests.get(artist_lookup)
-        artist_info = artist_response.json()
-        return artist_info
 
     @classmethod
     def _query_artist(cls, artist: str) -> dict:
@@ -45,106 +22,109 @@ class MusicMetadata:
         return artist_list
 
     @classmethod
-    def _pick_release(cls, release_group: str) -> dict:
-        release_id = release_group["id"]
-        release_lookup = (
-            f"{cls.base_url}release/{release_id}?inc=artists+recordings&fmt=json"
-        )
-        release_response = requests.get(release_lookup)
-        release = release_response.json()
+    def _pick_artist(cls, artists_list: list[dict]) -> dict:
+        artist = None
+        for posible_artist in artists_list:
+            if not artist:
+                artist = posible_artist
+            elif artist["score"] < posible_artist["score"]:
+                artist = posible_artist
+        if artist and artist["score"] != 100:
+            raise ValueError("Artist not found, check your query")
+        return artist
+
+    @classmethod
+    def _artist_lookup(cls, artist_id: str) -> dict:
+        artist_lookup = f"{cls.base_url}artist/{artist_id}?inc=release-groups&fmt=json"
+        artist_response = requests.get(artist_lookup)
+        artist_info = artist_response.json()
+        return artist_info
+
+    @classmethod
+    def _pick_release_group(cls, artist: str, release_name: str) -> dict:
+        artist_releases = cls.get_artist_info(artist)["release-groups"]
+        my_release = {}
+        for release in artist_releases:
+            if release["title"].lower() == release_name.lower():
+                my_release = release
+        if not my_release:
+            raise ValueError("No release found, check release name.")
+        return my_release
+
+    @classmethod
+    def _pick_release(cls, release_group_id: str) -> dict:
+        lookup = f"{cls.base_url}release-group/{release_group_id}?inc=releases&fmt=json"
+        response = requests.get(lookup)
+        release_groups = response.json()
+        release = release_groups["releases"][0]
         return release
 
     @classmethod
-    def _release_lookup(cls, release_group_id: str) -> dict:
-        release_lookup = (
-            f"{cls.base_url}release-group/{release_group_id}?inc=releases&fmt=json"
-        )
-        release_response = requests.get(release_lookup)
-        release_info = release_response.json()["releases"][0]
-        return release_info
+    def _release_lookup(cls, release_id: str) -> dict:
+        lookup = f"{cls.base_url}release/{release_id}?inc=artists+recordings+recording-rels+recording-level-rels+artist-rels&fmt=json"
+        response = requests.get(lookup)
+        release = response.json()
+        return release
+
+    @classmethod
+    def _pick_title(cls, titles_list: list, title: str) -> dict:
+        title_info = {}
+        for item in titles_list:
+            if item["title"].lower() == title.lower():
+                title_info = item
+        if not title_info:
+            raise ValueError("No title found, check title name.")
+        return title_info
+
+    @classmethod
+    def _pick_title_relations(cls, relations_list: list) -> dict:
+        relations = {
+                "recording engineer": [],
+                "engineer": [],
+                "producer": [],
+                "vocals": [],
+                "instruments": []
+                }
+        for relation in relations_list:
+            if relation["type"] == "recording":
+                relations["recording engineer"].append(relation)
+            elif relation["type"] == "engineer":
+                relations["engineer"].append(relation)
+            elif relation["type"] == "producer":
+                relations["producer"].append(relation)
+            elif relation["type"] == "vocal":
+                relations["vocals"].append(relation)
+            elif relation["type"] == "instrument":
+                relations["instruments"].append(relation)
+        return relations
 
     @classmethod
     def get_artist_info(cls, artist: str) -> dict:
-        artist_list = cls._query_artist(artist)
-        artist_pick = cls._pick_artist(artist_list)
-        my_artist = cls._artist_lookup(artist_pick["id"])
+        artists_list = cls._query_artist(artist)
+        artist_id = cls._pick_artist(artists_list)["id"]
+        my_artist = cls._artist_lookup(artist_id)
         return my_artist
 
     @classmethod
-    def get_albums(cls, artist: str) -> list:
-        release_groups = cls.get_artist_info(artist)["release-groups"]
-        albums = []
-        for release in release_groups:
-            if release["primary-type"] == "Album":
-                release_lookup = cls._release_lookup(release["id"])
-                release_pick = cls._pick_release(release_lookup)
-                albums.append(release_pick)
-                sleep(1)
-        __import__('pprint').pprint(albums)
-        return albums
+    def get_release(cls, artist: str, release: str) -> dict:
+        release_group = cls._pick_release_group(artist=artist, release_name=release)
+        release_info = cls._pick_release(release_group_id=release_group["id"])
+        release = cls._release_lookup(release_id=release_info["id"])
+        return release
 
     @classmethod
-    def get_singles(cls, artist: str) -> list:
-        release_groups = cls.get_artist_info(artist)["release-groups"]
-        singles = []
-        for release in release_groups:
-            if release["primary-type"] == "Single":
-                release_lookup = cls._release_lookup(release["id"])
-                release_pick = cls._pick_release(release_lookup)
-                singles.append(release_pick)
-                sleep(1)
-        return singles
+    def get_title(cls, artist: str, release: str, title: str) -> dict:
+        release = cls.get_release(artist=artist, release=release)
+        release_titles = release["media"][0]["tracks"]
+        title = cls._pick_title(titles_list=release_titles, title=title)
+        return title
 
     @classmethod
-    def get_episodes(cls, artist: str) -> list:
-        release_groups = cls.get_artist_info(artist)["release-groups"]
-        episodes = []
-        for release in release_groups:
-            if release["primary-type"] == "EP":
-                release_lookup = cls._release_lookup(release["id"])
-                release_pick = cls._pick_release(release_lookup)
-                episodes.append(release_pick)
-                sleep(1)
-        return episodes
-
-    @classmethod
-    def pick_recording(cls, recordings_list: list, artist: str, release: str, title: str) -> dict:
-        my_recording = {}
-        while not my_recording:
-            for recording in recordings_list:
-                recording_title = recording["title"].lower()
-                recording_artist = recording["artist-credit"][0]["name"].lower()
-                release_title = recording["releases"][0]["release-group"]["title"].lower()
-                if recording_artist == artist:
-                    if release_title == release:
-                        if recording_title == title:
-                            my_recording = recording
-        return my_recording
-
-    @classmethod
-    def query_song(cls, song: str) -> dict:
-        song = song.split()
-        song = "%20".join(song)
-        song_search = f"{cls.base_url}recording/?query={song}&fmt=json"
-        response = requests.get(song_search)
-        response_list = response.json()["recordings"]
-        return response_list
-
-    @classmethod
-    def lookup_recording(cls, recording: dict) -> dict:
-        recording_id = recording["id"]
-        recording_lookup = f"{cls.base_url}recording/{recording_id}?inc=artists+releases+isrcs+url-rels&fmt=json"
-        lookup_response = requests.get(recording_lookup)
-        recording = lookup_response.json()
-        return recording
-
-    @classmethod
-    def get_recording(cls, artist: str, release: str, title: str):
-        recordings_list = cls.query_song(title)
-        pick_recording = cls.pick_recording(recordings_list, artist, release, title)
-        recording = cls.lookup_recording(pick_recording)
-        breakpoint()
-        return recording
+    def get_title_relations(cls, artist: str, release: str, title: str) -> dict:
+        title = cls.get_title(artist=artist, release=release, title=title)
+        relations_list = title["recording"]["relations"]
+        relations = cls._pick_title_relations(relations_list)
+        return relations
 
 
 # --------------------------------------------------
@@ -152,9 +132,12 @@ def main() -> dict:
     """Main function for program"""
 
     client = MusicMetadata()
-    client.get_recording(artist="kings of convenience", release="declaration of dependence", title="rule my world")
+    artist = client.get_artist_info("kings of convenience")
+    release = client.get_release(artist="kings of convenience", release="quiet is the new loud")
+    title = client.get_title(artist="kings of convenience", release="quiet is the new loud", title="toxic girl")
+    title_relations = client.get_title_relations(artist="kings of convenience", release="quiet is the new loud", title="toxic girl")
+    breakpoint()
 
 
-# --------------------------------------------------
 if __name__ == "__main__":
     main()
