f8a4134524bc983b3981313363543a80684059b7
Salvador Estrella <seodent.qro@gmail.com>
2022-10-13 14:02:51 -0500

    fix: Remove shebang and imports from __main__.py

--- metamusic/__main__.py
+++ metamusic/__main__.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python3
 """
 Author : Salvador <Salvador@fedora>
 Date   : 2022-10-07
 Purpose: New program
 """
-
-from .clients import musicbrainz
-from .cli import 
-
-
-# --------------------------------------------------
-if __name__ == '__main__':
-    musicbrains.main("kings of convenience")
