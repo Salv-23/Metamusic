04b01eaee857f637705481b397e30c785cc1c5b2
Salvador <seodent.qro@gmail.com>
2022-11-28 12:30:43 -0600

    feat: remove typer functionality, add docstrings.

--- metamusic/clients/artwork.py
+++ metamusic/clients/artwork.py
@@ -2,18 +2,29 @@
 """
 Author : root <root@localhost>
 Date   : 2022-11-16
-Purpose: Program to retrieve HD artwork from iTunes
+Purpose: Program to retrieve and display HD artwork in the terminal.
 """
 
 from term_image.image import from_url
 import requests
 import re
-import typer
 
 
-app = typer.Typer()
 # --------------------------------------------------
-def get_release_object(release: str, artist: str) -> dict:
+def get_release_information(release: str, artist: str) -> dict:
+    """Find release information for a given release-artist name."
+
+    Args:
+        release (str): The name of the release to retrieve
+        artist (str): The name of the release artist
+
+    Raises:
+        ValueError: When the release information is not found
+
+    Returns:
+        dict: Useful release information such as URLs for release artwork.
+    """
+
     release_info = {}
     url = "https://artwork.themoshcrypt.net/api/search?keyword="
     release_query = "%2B".join(release.split()).lower()
@@ -33,6 +44,19 @@ def get_release_object(release: str, artist: str) -> dict:
 
 # --------------------------------------------------
 def get_high_definition_artwork_url(release_object: str) -> str:
+    """Fetches an URL for a high-definition artwork.
+
+    Retrieves the release path from a low-definition artwork URL
+    and constructs the URL for a high-definition artwork.
+
+    Args:
+        release_object (str):
+            A release object returned from the get_release_information function.
+
+    Returns:
+        str: An URL for the high-definition artwork.
+    """
+
     high_definition_url = "https://s1.mzstatic.com/us/r1000/063/"
     low_definition_url = release_object["artworkUrl100"]
     pattern = re.compile("Music.+(\.jpg)(?=/)|Music.+(\.png)(?=/)")
@@ -44,14 +68,19 @@ def get_high_definition_artwork_url(release_object: str) -> str:
     elif png_artwork_directory:
         return high_definition_url + png_artwork_directory
     else:
-        raise ValueError("No high definition artwork found")
+        raise ValueError("No artwork found, check release_object and try again.")
 
 
 # --------------------------------------------------
-@app.command()
-def display_artwork(release: str, artist: str):
+def main(release: str, artist: str):
+    """Display artwork for a given release and artist in the terminal.
 
-    release_object = get_release_object(release=release, artist=artist)
+    Args:
+        release (str): The name of the release.
+        artist (str): The name of the artist.
+    """
+
+    release_object = get_release_information(release=release, artist=artist)
     artwork_url = get_high_definition_artwork_url(release_object)
     image = from_url(artwork_url)
     image.draw()
@@ -59,4 +88,4 @@ def display_artwork(release: str, artist: str):
 
 # --------------------------------------------------
 if __name__ == "__main__":
-    app()
+    main()
