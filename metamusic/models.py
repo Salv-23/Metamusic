7eeb8fbe1ca65e15bd01aab75b122545d93bc05c
Salvador <seodent.qro@gmail.com>
2022-12-01 10:26:41 -0600

    fix: rename "album" to "release"

--- metamusic/models.py
+++ metamusic/models.py
@@ -6,8 +6,8 @@ from dataclasses import dataclass
 class Track:
     artist: str
     title: str
-    album: str
-    album_type: T.Optional[str] = None
+    release: str
+    release_type: T.Optional[str] = None
     track_number: T.Optional[int] = None
     total_tracks: T.Optional[int] = None
     recording_date: T.Optional[str] = None
