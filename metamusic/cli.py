9054c563f73d30249fc0db18f4fc42090d77f448
Salvador <seodent.qro@gmail.com>
2022-12-05 12:03:48 -0600

    feat: cli first model

--- metamusic/cli.py
+++ metamusic/cli.py
@@ -4,25 +4,35 @@ Date   : 2022-10-05
 Purpose: Command-line interface for metamusic
 """
 
-import argparse
+import typer
+from pprint import pprint as pp
+from clients import (
+    MusicBrainz,
+    high_definition_artwork,
+    get_track_tags,
+    set_selected_tags,
+    set_all_tags,
+    set_artwork_locally,
+    set_artwork_from_url,
+)
+
+app = typer.Typer()
 
 
-# --------------------------------------------------
-def get_args():
-    """Get command-line arguments"""
+@app.command()
+def set_metadata(track_file: str, artist: str, release: str, title: str):
+    pass
 
-    parser = argparse.ArgumentParser(
-        description="Retrieve arguments to work with the cli",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
 
-    parser.add_argument(
-        "-a",
-        "--artist",
-        help="An artist to search for",
-        metavar="str",
-        type=str,
-        required=True,
-    )
+@app.command()
+def set_artwork(track_file: str, artwork_file: str):
+    files = find_all_valid_files(directory)
+    for file in files:
+        handler = FileHandler(file)
+        handler.get_artwork = include_artwork
+        handler.get_metadata()
+        handler.save_metadata()
 
-    return parser.parse_args()
+
+if __name__ == "__main__":
+    app()
